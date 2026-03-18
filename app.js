89e1cb908d39bbb64c19b45af2ecb26549df7130
Prajwalit Mohanty <pm.pranjalmohanty125@gmail.com>
2026-03-19 01:56:19 +0530

    both of the next and prev buttons are finally working, solved major bugs in the js file and also tweaked it a little to boost optimization

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ let thumbnailDom = document.querySelector('.carousel .thumbnail');
 nextDom.onclick = function(){
    showSlider('next');
 }
+prevDom.onclick = function() {
+   showSlider('prev');
+}
+
+let timeRunning = 3000;
+let runTimeOut;
 function showSlider(type){
    let itemSlider = document.querySelectorAll('.carousel .list .item');
    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
@@ -35,15 +41,16 @@ function showSlider(type){
       listItemDom.appendChild(itemSlider[0]);
       thumbnailDom.appendChild(itemThumbnail[0]);
       carouselDom.classList.add('next');
+   }else{
+      let positionLastItem = itemSlider.length - 1;
+      listItemDom.prepend(itemSlider[positionLastItem]);
+      thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+      carouselDom.classList.add('prev');
    }
 
    setTimeout(() => {
       carouselDom.classList.remove('next');
       carouselDom.classList.remove('prev');
    }, 2000);
-
-   
-
-
 
 }
