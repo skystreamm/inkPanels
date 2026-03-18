95d3034c61eb8e11cea3c75540f6dee92402cdce
Prajwalit Mohanty <pm.pranjalmohanty125@gmail.com>
2026-03-18 23:16:01 +0530

    Added the required JS scripts to make the next button functionable, fixed a proper reset wrror of the animations glitching by using a settimeout to remove next and prev classes from the carousel container

--- app.js
+++ app.js
@@ -18,3 +18,32 @@ if(navClose){
 }
 
 // =============CAROUSEL AND ITS RESPECTIVE THUMBNAIL========== //
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function(){
+   showSlider('next');
+}
+function showSlider(type){
+   let itemSlider = document.querySelectorAll('.carousel .list .item');
+   let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+   if(type === 'next'){
+      listItemDom.appendChild(itemSlider[0]);
+      thumbnailDom.appendChild(itemThumbnail[0]);
+      carouselDom.classList.add('next');
+   }
+
+   setTimeout(() => {
+      carouselDom.classList.remove('next');
+      carouselDom.classList.remove('prev');
+   }, 2000);
+
+   
+
+
+
+}
